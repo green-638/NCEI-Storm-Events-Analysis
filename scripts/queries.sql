7346983e02bcbc02624742758f732d3904056e21
Connor Hall <connor582h@gmail.com>
2026-04-08 19:28:29 -0400

    created views from queries

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -1,4 +1,5 @@
 -- query 1: total weather event count per year
+CREATE VIEW annual_event_count_view AS
 SELECT
 	CAST(YEAR(begin_date) AS CHAR(4)) AS `year`,
 	COUNT(event_id) AS event_count
@@ -8,9 +9,17 @@ JOIN time
 		USING(time_id)
 GROUP BY
 	CAST(YEAR(begin_date) AS CHAR(4));
+
+
+-- query 2: total damage
+CREATE VIEW total_damage_view AS
+SELECT SUM(damage_property + damage_crops) AS total_cost
+FROM events
+JOIN event_details USING(details_id)  
 	
 	
--- query 2: average weather event count per month
+-- query 3: average weather event count per month
+CREATE VIEW avg_monthly_event_count_view AS
 WITH q3_cte AS (
 	SELECT
 		YEAR(begin_date) AS `year`,
@@ -34,8 +43,8 @@ GROUP BY
 	`month`;
 
 
--- query 3: most common weather events overall counted in each state 
-CREATE VIEW state_weather_events AS
+-- query 4: # of weather events in each state
+CREATE VIEW state_event_count_view AS
 SELECT
 	state,
 	event_type,
@@ -49,21 +58,19 @@ JOIN locations
 GROUP BY
 	state,
 	event_type
-HAVING
-	event_type IN ('Thunderstorm Wind', 'Hail',
-	'Flash Flood', 'High Wind', 'Winter Weather')
 ORDER BY
 	state;
 	
 	
--- query 4: avg damage per minute for each event type
+-- query 5: avg damage per minute for each event type
+CREATE VIEW avg_damage_per_min AS
 WITH q4_cte AS (
 SELECT
 		event_type,
+		TIMESTAMPDIFF(MINUTE, begin_date, end_date) AS minutes,
 		damage_property + damage_crops AS total_damage,
-		begin_date,
-		end_date,
-		TIMESTAMPDIFF(MINUTE, begin_date, end_date) AS minutes
+		(damage_property + damage_crops) / 
+			TIMESTAMPDIFF(MINUTE, begin_date, end_date) AS damage_per_minute
 FROM
 		events
 JOIN time
@@ -73,13 +80,11 @@ JOIN event_details
 WHERE
 		TIMESTAMPDIFF(MINUTE, begin_date, end_date) > 0
 	AND damage_property + damage_crops > 0
-ORDER BY
-		end_date - begin_date DESC
 )
 
 SELECT
 	event_type,
-	ROUND(AVG(total_damage / minutes), 2) AS damage_per_minute
+	ROUND(AVG(damage_per_minute), 2) AS avg_damage_per_minute
 FROM
 	q4_cte
 GROUP BY
@@ -88,36 +93,4 @@ GROUP BY
 HAVING
 	COUNT(event_type) > 10
 ORDER BY
-	ROUND(AVG(total_damage / minutes), 2) DESC;
-
-
--- query 5: avg injuries/deaths per hour for each event type
-WITH q5_cte AS (
-SELECT
-		event_type,
-		injuries_direct + injuries_indirect + deaths_direct + deaths_indirect AS total,
-		begin_date,
-		end_date,
-		TIMESTAMPDIFF(HOUR, begin_date, end_date) AS hours
-FROM
-		events
-JOIN time
-		USING(time_id)
-JOIN event_details
-		USING(details_id)
-WHERE
-		TIMESTAMPDIFF(HOUR, begin_date, end_date) > 0
-	AND injuries_direct + injuries_indirect + deaths_direct + deaths_indirect > 0
-ORDER BY
-		end_date - begin_date DESC
-)
-
-SELECT
-	event_type,
-	ROUND(AVG(total / hours), 2) AS accidents_per_hour
-FROM
-	q5_cte
-GROUP BY
-	event_type
-ORDER BY
-	ROUND(AVG(total / hours), 2) DESC;
+	ROUND(AVG(damage_per_minute), 2) DESC;
